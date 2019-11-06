011310f3f9e671884879ced90c92165de93f74d8
SamMeyerson <sammeyerson@ufl.edu>
2019-11-06 15:48:53 -0500

    basic header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import home from '../../assets/home-alt.png';
+
 
 const Header = () => {
     return (
+    <div className='container-fluid'>
+      <div class="col-xl">
         <div className='topnav'>
             {/* Logo */}
             <Link id="logo-link" to="/">
-                <img className="topnav-logo" src={ "/logo192.png" } alt="React logo" />
+                <img className="topnav-logo" src={ home } alt="Home" />
             </Link>
-
-            {/* Page Links */}
-            <div className="topnav-right">
-                <Link className="topnav-link" to='/Register'>Sign in</Link>
-                <Link className="topnav-link" to='/projects'>Projects</Link>
-                <a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://www.facebook.com/groups/ufosc/events/?source=4&action_history=null&filter=calendar">
-                    Events
-                    <i className="fas fa-external-link-alt external-link" data-fa-transform="up-6"></i>
-                </a>
-                <a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://github.com/ufosc/club-resources">
-                    Resources
-                    <i className="fas fa-external-link-alt external-link" data-fa-transform="up-6 right-4"></i>
-                </a>
-                <Link className="topnav-link" to="/about">About</Link>
-            </div>
+              <p className="top-name">myGNV Resource Finder</p>
         </div>
+      </div>
+    </div>
+
     )
 }
 
