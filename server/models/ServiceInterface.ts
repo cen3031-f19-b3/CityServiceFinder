572244222a8630b09f3d911bb8e746f2d57abba2
Hunter Jarrell <hjarrell555@gmail.com>
2019-11-11 22:33:28 -0500

    Convert service schema to typescript

---
+++ server/models/ServiceInterface.ts
@@ -0,0 +1,60 @@
+import { Document, Types } from 'mongoose';
+import ICategory from './CategoryInterface';
+
+export interface IAddress extends Types.Subdocument {
+    line_1: string;
+    line_2?: string;
+    city: string;
+    state: string;
+    zip: string;
+}
+
+export interface IPhoneNumber extends Types.Subdocument {
+    contact?: string;
+    number: string;
+}
+
+export interface IHour extends Types.Subdocument {
+    start?: Date;
+    end?: Date;
+    note?: string;
+}
+
+export interface IAppointment extends Types.Subdocument {
+    is_required: boolean;
+    phone: string;
+    website: string;
+    email: string;
+    other_info: string;
+}
+
+export interface IApplication extends Types.Subdocument {
+    is_required: boolean;
+    apply_online: boolean;
+    apply_in_person: boolean;
+    phone: string;
+    website: string;
+    email: string;
+    other_info: string;
+}
+
+export interface IService extends Document {
+    name: string;
+    addresses: IAddress[];
+    phone_numbers: IPhoneNumber[];
+    services_freeform?: string;
+    eligibility_criteria?: string;
+    emails: string[];
+    bus_routes: string[];
+    website: string[];
+    walk_ins: string;
+    hours: IHour[];
+    appointment: IAppointment;
+    service_area: string;
+    application: IApplication;
+    cost_info: string;
+    translation_available: boolean;
+    united_way_approval: boolean;
+    additional_information: string;
+    categories: ICategory[];
+}
---
+++ server/models/ServiceSchema.ts
@@ -0,0 +1,88 @@
+import { ObjectId } from 'mongodb';
+import mongoose, { Schema } from 'mongoose';
+
+import { IService } from './ServiceInterface';
+
+// tslint:disable: object-literal-sort-keys
+const serviceSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  addresses: [
+    {
+      line_1: {
+        type: String,
+        required: true
+      },
+      line_2: String,
+      city: {
+        type: String,
+        required: true
+      },
+      state: {
+        type: String,
+        required: true
+      },
+      zip: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  phone_numbers: [
+    {
+      contact: {
+        type: String,
+        required: false
+      },
+      number: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  services_freeform: {
+    type: String,
+    required: false
+  },
+  eligibility_criteria: {
+    type: String,
+    required: false
+  },
+  emails: [String],
+  bus_routes: [String],
+  website: [String],
+  walk_ins: String,
+  hours: [
+    {
+      start: Date,
+      end: Date,
+      note: String
+    }
+  ],
+  appointment: {
+    is_required: Boolean,
+    phone: String,
+    website: String,
+    email: String,
+    other_info: String
+  },
+  service_area: String,
+  application: {
+    is_required: Boolean,
+    apply_online: Boolean,
+    apply_in_person: Boolean,
+    phone: String,
+    website: String,
+    email: String,
+    other_info: String
+  },
+  cost_info: String,
+  translation_available: Boolean,
+  united_way_approval: Boolean,
+  additional_information: String,
+  categories: [ObjectId]
+});
+
+export default mongoose.model<IService>('Service', serviceSchema);
