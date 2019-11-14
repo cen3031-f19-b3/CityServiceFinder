6632715d06ba0bfb0f1fc124dee11137f6044f4e
Alex Shuping <alex.g.shuping@gmail.com>
2019-11-14 14:53:38 -0500

    Update images and text to match style guide

--- client/src/components/CategoryPage.js
+++ client/src/components/CategoryPage.js
@@ -26,14 +26,14 @@ function CategoryPage()
                     </p>
 										<div className="d-flex flex-wrap">
 											<ServiceButton text={"Family Services"} img={"child"} link="./cat/childandfamilies" />
-											<ServiceButton text={"Education"} img={"school"} link="./cat/education" />
-											<ServiceButton text={"Finance"} img={"money-bill-alt"} link="./cat/financials" />
-											<ServiceButton text={"Health and Wellness"} img={"briefcase-medical"} link="./cat/healthandwellness" />
-											<ServiceButton text={"Jobs"} img={"address-card"} link="./cat/job" />
-											<ServiceButton text={"Legal Services"} img={"balance-scale"} link="./cat/legal" />
-											<ServiceButton text={"Crisis Services"} img={"thunderstorm"} link="./cat/crisisevents" />
-											<ServiceButton text={"Transportation"} img={"bus-alt"} link="./cat/transportation" />
-											<ServiceButton text={"Basic Needs"} img={"handshake"} link="./cat/basicneeds" />
+											<ServiceButton text={"Education"} img={"book"} link="./cat/education" />
+											<ServiceButton text={"Finance"} img={"money-check-edit"} link="./cat/financials" />
+											<ServiceButton text={"Health Services"} img={"briefcase-medical"} link="./cat/healthandwellness" />
+											<ServiceButton text={"Jobs"} img={"clipboard"} link="./cat/job" />
+											<ServiceButton text={"Legal Services"} img={"landmark"} link="./cat/legal" />
+											<ServiceButton text={"Crisis Services"} img={"shield"} link="./cat/crisisevents" />
+											<ServiceButton text={"Transport"} img={"bus-alt"} link="./cat/transportation" />
+											<ServiceButton text={"Basic Needs"} img={"utensils-alt"} link="./cat/basicneeds" />
 											<ServiceButton text={"Other"} img={"ellipsis-v"} link="./cat/other" />
 										</div>
              </main>
