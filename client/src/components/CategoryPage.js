9887bf56f61f6a39b5ac31e05917333b170464b1
Alex Isabelle Shuping <alex.g.shuping@gmail.com>
2019-11-19 10:17:59 -0500

    Modify css to remove absolute positioning
    ~ Modify CategoryPage.css to remove absolute positioning and handle element positioning relatively instead.
    ~ Modify CategoryPage.js to better support relative positioning.
    ~ Modify CategoryPage.css to use color-change instead of margin-growth to indicate hovered-over buttons.

--- client/src/components/CategoryPage.js
+++ client/src/components/CategoryPage.js
@@ -4,76 +4,77 @@ import { GetAllCategories } from '../util/Categories'
 import './CategoryPage.css';
 
 function ServiceButton({ text, img, link }) {
-	var img_txt = "fal fa-" + img + " fa-3x"
-	return (
-		<a className="service-button" href={link}>
-			<div className="service-button-main">
-				<p><i className={img_txt} /></p>
-				<p>{text}</p>
-			</div>
-		</a>
-	)
+  var img_txt = "fal fa-" + img + " fa-3x"
+  return (
+    <a className="service-button" href={link}>
+      <div className="service-button-main">
+        <p><i className={img_txt} /></p>
+        <p>{text}</p>
+      </div>
+    </a>
+  )
 }
 
 function CategoryPage() {
-	const [data, set_data] = useState([])
-	const [load_done, set_load_done] = useState(false)
+  const [data, set_data] = useState([])
+  const [load_done, set_load_done] = useState(false)
 
-	if (!load_done) {
-		GetAllCategories()
-			.then((categories) => {
-				set_data(categories)
-				set_load_done(true)
-			})
-			.catch((e) => console.error(e))
+  if (!load_done) {
+    GetAllCategories()
+      .then((categories) => {
+        set_data(categories)
+        set_load_done(true)
+      })
+      .catch((e) => console.error(e))
 
-		return (
-			<div className="cat-page">
-				<main>
-					<h1>Loading...</h1>
-				</main>
-			</div>
-		)
-	}
+    return (
+      <div className="cat-page">
+        <main>
+          <h1>Loading...</h1>
+        </main>
+      </div>
+    )
+  }
 
-	const categories_list = data
-		.filter(category => {
-			return category["subcategory_of"].length === 0
-		})
-		.map(category => {
-			return (
-				<ServiceButton
-					text={category.name}
-					img={category.img}
-					link={"./cat/" + category.link}
-				/>
-			)
-		})
+  const categories_list = data
+    .filter(category => {
+      return category["subcategory_of"].length === 0
+    })
+    .map(category => {
+      return (
+        <ServiceButton
+          text={category.name}
+          img={category.img}
+          link={"./cat/" + category.link}
+        />
+      )
+    })
 
-	return (
-		
-		<div className="cat-page">
+  return (
+    
+    <div className="cat-page">
 
-			<main>
+      <main>
 
-				<div className="d-flex flex-wrap">
-					{categories_list}
-				</div>
-				
-				<i className="heart fal fa-hand-holding-heart fa-7x" />
+        <div className="intro-text d-flex flex-wrap">
+          <div className="header-image">
+            <i className="heart fal fa-hand-holding-heart fa-7x" />
+          </div>
+          <div className="body-text">
+            <p>Life can get tough sometimes. We get it. We're here to help. Find free resources here.</p>
+          </div>
+        </div>
 
-                <div className="body-text">
-                    <p>Life can get tough sometimes.
-                        We get it. We're here to help.
-                        Find free resources here.
-                    </p>
-                </div>
+        <div className="d-flex flex-wrap">
+          {categories_list}
+        </div>
+        
 
-			</main>
+      </main>
 
 
-		</div>
-	)
+    </div>
+  )
 }
 
 export default CategoryPage;
