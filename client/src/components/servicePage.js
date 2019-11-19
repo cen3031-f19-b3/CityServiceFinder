c631086bbd2f9fc22ee1d7d482e8d2a32f454997
SamMeyerson <sammeyerson@ufl.edu>
2019-11-19 14:49:25 -0500

    Added functions to return data on service

--- client/src/components/servicePage.js
+++ client/src/components/servicePage.js
@@ -3,13 +3,10 @@ import {GetAllServices} from '../util/Services'
 import './servicePage.css';
 
 
-
 function FindAddress({service}){
+
   const numOfAddresses=Object.keys(service.addresses).length
-
-
-
-
+  var addresses=[]
   if(numOfAddresses===0){
     return(
       <div>
@@ -19,57 +16,230 @@ function FindAddress({service}){
     )
   }
   else{
-      const addL1= service.addresses[0].line_1
+
+    for(var i=0;i<numOfAddresses;i++){
+      var address=""
+
+      var addL1= service.addresses[i].line_1
       var addL2=""
-      if(Object.keys(service.addresses[0].line_2).length>0){
-        addL2 =service.addresses[0].line_2
+      if(!service.addresses[i].line_2){
+
+      }
+      else{
+        if(Object.keys(service.addresses[i].line_2).length>0){
+          addL2 =service.addresses[i].line_2
+        }
       }
 
-
-      const city= service.addresses[0].city
-      const state= service.addresses[0].state
-      const zip= service.addresses[0].zip
-
+      var city= service.addresses[i].city
+      var state= service.addresses[i].state
+      var zip= service.addresses[i].zip
+      address=address.concat(addL1,' ', addL2,' ', city,', ', state,' ', zip,'. ')
+      addresses.push(address)
+    }
+    console.log('addresses: ',addresses)
     return(
       <div>
-      {addL1} {addL2} {city}, {state} {zip}
+        { addresses.map((address, index)=>(
+          <div key={index} >
+            {address}
+          </div>
+        )) }
       </div>
     )
 
   }
+}
 
-/*  const addL1= service.addresses[0].line_1
-  const addL2= service.addresses[0].line_2
-  const city= service.addresses[0].city
-  const state= service.addresses[0].state
-  const zip= service.addresses[0].zip*/
-  /*if (addL1===null){
+function FindEmails({service}){
+
+  const numOfEmails=Object.keys(service.emails).length
+  var emails=[]
+  if(numOfEmails===0){
     return(
       <div>
-        This service has no address
+        There are no emails listed for this service.
       </div>
+
     )
   }
   else{
 
+    var email
+    for(var i=0; i<numOfEmails; i++){
+      email=service.emails[i]
+      emails.push(email)
+
+    }
+    console.log('emails: ',emails)
+
     return(
       <div>
-        <p>
-
-
-        </p>
-
+        { emails.map((email, index)=>(
+          <div key={index} >
+            {email}
+          </div>
+        )) }
       </div>
     )
-  }*/
+
+  }
+}
+
+function FindCost({service}){
+
+  if(service.cost_info){
+    console.log('cost info: ',service.cost_info)
+    return(
+      <div>
+        {service.cost_info}
+      </div>
+
+    )
+  }
+
   return(
-    <div>default return
-    {numOfAddresses}
+    <div>
+      No cost info
     </div>
   )
+}
+
+function FindWebsite({service}){
+
+  const numOfWebsites=Object.keys(service.website).length
+  var websites=[]
+  if(numOfWebsites===0){
+    return(
+      <div>
+        There is no website listed for this service.
+      </div>
+
+    )
+  }
+  else{
+
+    var website
+    for(var i=0; i<numOfWebsites; i++){
+      website=service.website[i]
+      websites.push(website)
+
+    }
+    console.log('websites: ',websites)
+
+    return(
+      <div>
+        { websites.map((website, index)=>(
+          <div key={index}><a href={website} target="_blank" rel="noopener noreferrer">{website}</a></div>
+        )) }
+      </div>
+    )
+  }
+}
+
+function FindBusRoutes({service}){
+
+  const numOfBusRoutes=Object.keys(service.bus_routes).length
+  var busRoutes=[]
+  if(numOfBusRoutes===0){
+    return(
+      <div>
+        There are no bus routes listed for this service.
+      </div>
+
+    )
+  }
+  else{
+
+    var route
+    for(var i=0; i<numOfBusRoutes; i++){
+      route=service.bus_routes[i]
+      busRoutes.push(route)
+
+    }
+    console.log('bus routes: ',busRoutes)
+
+    return(
+      <div>
+        { busRoutes.map((route, index)=>(
+          <div key={index}>{route}</div>
+        )) }
+      </div>
+    )
+
+  }
 
 }
 
+
+function FindHours({service}){
+
+  const numOfDaysOpen=Object.keys(service.hours).length
+  var hours=[]
+  if(numOfDaysOpen===0){
+    return(
+      <div>
+        There are no hours listed for this service.
+      </div>
+
+    )
+  }
+  else{
+
+    var day
+    for(var i=0; i<numOfDaysOpen; i++){
+      day=service.hours[i]
+      hours.push(day)
+
+    }
+    console.log('hours: ',hours)
+
+    return(
+      <div>
+        { hours.map((day, index)=>(
+          <div key={index}>{day}</div>
+        )) }
+      </div>
+    )
+  }
+}
+
+function FindPhoneNumber({service}){
+  
+  const numOfPhoneNums=Object.keys(service.phone_numbers).length
+  var numbers=[]
+  if(numOfPhoneNums===0){
+
+    return(
+      <div>
+        There is no phone number listed for this service.
+      </div>
+
+    )
+  }
+  else{
+
+    var num
+    for(var i=0; i<numOfPhoneNums; i++){
+      num=service.phone_numbers[i].number
+      if(service.phone_numbers[i].contact){
+        num.concat(': ',service.phone_numbers[i].contact)
+      }
+      numbers.push(num)
+
+    }
+    console.log('phone numbers: ',numbers)
+
+    return(
+      <div>
+        { numbers.map((number, index)=>(
+          <div key={index}>{number}</div>
+        )) }
+      </div>
+    )
+  }
+
+}
 
 function FindService({data, name, loading_done}){
 
@@ -89,25 +259,49 @@ function FindService({data, name, loading_done}){
 	}
 
   const service=data.find(service=> service.name===name);
-
+  console.log('name: ',service.name)
   return(
-    <div>
+    <div className="text-center">
       <h2 className="text-center">{service.name}</h2>
-      <li>{service.services_freeform}</li>
-
-      <li>{service.phone_numbers[0].number}</li>
-      <li>{service.eligibility_criteria_freeform}</li>
-      <li>Hours:{service.hours[0]}</li>
-      <FindAddress
-        service={service}
-      />
+      <li>Address:
+        <FindAddress
+          service={service}
+        />
+      </li>
+      <li>Phone Number:
+        <FindPhoneNumber
+          service={service}
+        />
+      </li>
+      <li>Hours:
+        <FindHours
+          service={service}
+        />
+      </li>
+      <li>Emails:
+        <FindEmails
+          service={service}
+        />
+      </li>
+      <li>Bus Routes:
+        <FindBusRoutes
+          service={service}
+        />
+      </li>
+      <li>Websites:
+        <FindWebsite
+          service={service}
+        />
+      </li>
+      <li>Cost Info:
+        <FindCost
+          service={service}
+        />
+      </li>
     </div>
   )
 
-
 }
-
-
 
 class ServicePage extends React.Component{
 
@@ -117,28 +311,24 @@ class ServicePage extends React.Component{
 			services: [],
 			loading_done: false
 		}
-
 		GetAllServices()
 			.then((services) => this.data_load_complete(services))
 			.catch((e) => console.error(e))
 	}
-
 	data_load_complete(services){
 		this.setState({
 			services: services,
 			loading_done: true
 		})
 	}
-
     render(){
         return (
-          <div>
+          <div className='container-fluid'>
                 <FindService
                   data={this.state.services}
                   name={this.props.name}
                   loading_done={this.state.loading_done}
                 />
-
           </div>
         )
     }
