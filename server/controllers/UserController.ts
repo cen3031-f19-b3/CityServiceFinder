5c4d6524c25f53236cb466403189ebef7abb0fba
Hunter Jarrell <hjarrell555@gmail.com>
2019-11-30 19:49:54 -0500

    Reformat UserController

--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -3,53 +3,56 @@ import passport from 'passport';
 import { UserModel } from '../models/UserSchema';
 
 export const RegisterUser = (req: Request, res: Response) => {
-    UserModel.register(new UserModel({email: req.body.email}), req.body.password, (err, user) => {
-        if (err) {
-            console.log(`Error registering user: ${err}`);
+  UserModel.register(
+    new UserModel({ email: req.body.email }),
+    req.body.password,
+    (error, topUser) => {
+      if (error) {
+        console.log(`Error registering user: ${error}`);
+        return res.status(500).json({
+          success: false
+        });
+      } else {
+        passport.authenticate('local', (err, user, info) => {
+          if (err) {
+            console.log(`Error authenticating just-registered user: ${err}`);
             return res.status(500).json({
-                success: false
-            })
+              success: false
+            });
+          }
 
-        } else {
-            passport.authenticate('local', (err, user, info) => {
-                if(err){
-                    console.log(`Error authenticating just-registered user: ${err}`);
-                    return res.status(500).json({
-                        success: false
-                    })
-                }
+          if (!user) {
+            return res.status(401).json({
+              success: false
+            });
+          }
 
-                if(!user){
-                    return res.status(401).json({
-                        success: false
-                    });
-                }
-
-                return res.json({
-                    success: true
-                })
-            })(req, res);
-        }
-    });
+          return res.json({
+            success: true
+          });
+        })(req, res);
+      }
+    }
+  );
 };
 
 export const LoginUser = (req: Request, res: Response) => {
-    passport.authenticate('local', (err, user, info) => {
-        if(err){
-            console.log(`Error authenticating user: ${err}`);
-            return res.status(500).json({
-                success: false
-            })
-        }
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.log(`Error authenticating user: ${err}`);
+      return res.status(500).json({
+        success: false
+      });
+    }
 
-        if(!user){
-            return res.status(401).json({
-                success: false
-            });
-        }
+    if (!user) {
+      return res.status(401).json({
+        success: false
+      });
+    }
 
-        return res.json({
-            success: true
-        });
-    })(req, res);
+    return res.json({
+      success: true
+    });
+  })(req, res);
 };
