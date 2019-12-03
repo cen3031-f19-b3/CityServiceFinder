c93dd80720bc8f5b98805e106ffaa3512d91c6e7
Alex Isabelle Shuping <alex.i.shuping@gmail.com>
2019-12-03 00:53:39 -0500

    MapView improvements

--- client/src/views/Map/MapView.js
+++ client/src/views/Map/MapView.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { Link, Redirect } from 'react-router-dom'
 import { GetCategoryServices } from '../../util/Services'
+import { GetSingleCategory } from '../../util/Categories'
+import SearchableList from '../../components/SearchableList/SearchableList'
+
 import './MapView.css'
-import { GetSingleCategory } from '../../util/Categories';
 
 const grab_services = async (cat_id) => {
 	return await GetCategoryServices(cat_id)
@@ -12,15 +14,11 @@ const grab_own_category = async (cat_id) => {
 	return await GetSingleCategory(cat_id)
 }
 
-function SingleServiceLi({service}){
-	return <li className="service-list-item">
-		<Link to={`/services/${service._id}`}>{service.name}{(service.addresses && service.addresses.length > 0) ? ` (${service.addresses[0].line_1})` : ""}</Link>
-	</li>
-}
-
 function MapView({cat_id}){
 	const [services, set_services] = useState(null)
 	const [cat, set_cat] = useState(null)
+
+	const [page_redir, do_redirect] = useState(null)
 
 	useEffect(() => {
 		grab_services(cat_id)
@@ -35,18 +33,35 @@ function MapView({cat_id}){
 	const category_name = (cat === null) ? <h1>Loading...</h1> : <h1>{cat.name}</h1>
 
 	const serv_list = (services === null) 
-		? <li className="service-list-loading">Loading...</li> 
+		? <h2>Loading...</h2> 
 		: ((services.length === 0) 
 			? <h2>No services found.</h2> 
-			: services.map((serv) => <SingleServiceLi key={serv._id} service={serv} />
+			: services.map((serv) => {
+				return {
+					contents: serv.name,
+					search_on: serv.name + serv._id,
+					_id: serv._id
+				}
+			}
 		)
 	)
 
 	return(<div className="map-page">
+		{page_redir}
+		{<p className="subtle-text"><Link to="/cat">All Categories ></Link></p>}
 		{category_name}
-		<ul>
-			{serv_list}
-		</ul>
+		{(services) ? <h2>The following services are available in this category:</h2> : null}
+		{(services) 
+			? <SearchableList 
+				objects={serv_list}
+				click_callback={(obj) => {
+					do_redirect(<Redirect path to={`/services/${obj._id}`} />)
+				}}
+				selectable={false}
+				multi_select={false}
+			/>
+			: serv_list
+		}
 	</div>)
 }
 
